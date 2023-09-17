59fd582bf5fe98f0fcca0a10f4c91b9e6d826f87
Alberto Moro <amc295@alumnos.unican.es>
2023-09-17 18:38:21 +0200

    Intrucciones ampliadas

--- AppRiesgosPersonales/app/src/main/java/es/unican/appriegospersonales/repository/rest/CibelServiceConstants.java
+++ AppRiesgosPersonales/app/src/main/java/es/unican/appriegospersonales/repository/rest/CibelServiceConstants.java
@@ -1,7 +1,7 @@
 package es.unican.appriegospersonales.repository.rest;
 
 public class CibelServiceConstants {
-    private static final String API_URL = "http://192.168.4.30:8080/";
+    private static final String API_URL = "http://192.168.1.53:8080/";
 
     public static String getAPIURL() {
         return API_URL;
