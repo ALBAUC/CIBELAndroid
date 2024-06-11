a5a5d2b1161ec99e0063cdf033ab06ccb9cd5591
Carlos <blancobc@unican.es>
2024-06-11 20:10:49 +0200

    movido servicio a otro repo

--- CIBELServiceSpring/src/main/java/es/unican/CIBEL/repository/TipoRepository.java
+++
@@ -1,9 +0,0 @@
-package es.unican.CIBEL.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.unican.CIBEL.domain.Tipo;
-
-public interface TipoRepository extends JpaRepository<Tipo, Long> {
-	Tipo findByNombre(String nombre);
-}
